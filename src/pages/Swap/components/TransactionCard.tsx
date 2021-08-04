17494bfea6e0aa34f3b7b9741680c8f9c600851f
totop716 <totop716@gmail.com>
2021-08-04 18:41:13 +0800

    update transaction table

--- src/pages/Swap/components/TransactionCard.tsx
+++ src/pages/Swap/components/TransactionCard.tsx
@@ -3,30 +3,6 @@ import styled from 'styled-components'
 import { Flex } from '@pancakeswap-libs/uikit'
 import moment from 'moment'
 import axios from 'axios';
-import DataTable, { createTheme } from 'react-data-table-component';
-
-
-createTheme('solarized', {
-  text: {
-    primary: '#268bd2',
-    secondary: '#2aa198',
-  },
-  background: {
-    default: '#002b36',
-  },
-  context: {
-    background: '#cb4b16',
-    text: '#FFFFFF',
-  },
-  divider: {
-    default: '#073642',
-  },
-  action: {
-    button: 'rgba(0,0,0,.54)',
-    hover: 'rgba(0,0,0,.08)',
-    disabled: 'rgba(0,0,0,.12)',
-  },
-});
 
 const data = [
 	{
@@ -70,13 +46,18 @@ const TableWrapper = styled.div`
 	background: rgba(0, 0, 0, 0.4);
 	border-radius: 8px;
 	height: 100%;
-	& .rdt_Table {
+	& table {
 		background: transparent;
-		& .rdt_TableHeadRow, & .rdt_TableRow {
+		width: 100%;
+		& tr {
 			background: transparent;
+			border-bottom: 1px solid rgba(255, 255, 255, 0.1);
 		}
-		& .rdt_TableHeadRow {
-			& .rdt_TableCol {
+		& td {
+			padding: 8px;
+		}
+		& thead {
+			& td {
 				color: white;
 				& > div > div {
 					font-size: 16px;
@@ -84,21 +65,16 @@ const TableWrapper = styled.div`
 				}
 			}
 		}
-		& .rdt_TableBody {
-			& .rdt_TableRow {
-				min-height: unset;
-				padding: 8px 0;
-				border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-				h2 {
-					font-size: 14px;
-					line-height: 16px;
-					font-weight: bold;
-					&.success {
-						color: #00AC1C;
-					}
-					&.error {
-						color: #EA3943;
-					}
+		& tbody {
+			h2 {
+				font-size: 14px;
+				line-height: 16px;
+				font-weight: bold;
+				&.success {
+					color: #00AC1C;
+				}
+				&.error {
+					color: #EA3943;
 				}
 			}
 		}
@@ -131,42 +107,7 @@ const ArrowUp = styled.div`
 const TransactionCard = () => {
   const [hideDirector, setHideDirector] = React.useState(false);
     
-//   const [alldata, setalldata] = useState([]);
-  const columns=[
-    {
-      name: 'Time',
-      selector: 'time',
-      sortable: true,
-			cell: (row, index) => <Flex alignItems='center'>{ moment().diff(moment(row.time), 'minute') >= 1 ? <ArrowDown /> : <ArrowUp /> }<h2 className={moment().diff(moment(row.time), 'minute') >= 1 ? 'error' : 'success'}>{ moment().diff(moment(row.time), 'minute') >= 1 ? moment(row.time).utc().fromNow() : 'just now' }</h2></Flex>
-    },
-    {
-      name: 'Traded',
-      selector: 'traded',
-      sortable: true,
-			cell: (row, index) => <h2 className={moment().diff(moment(row.time), 'minute') >= 1 ? 'error' : 'success'}>{ row.traded }</h2>
-    },
-    {
-      name: 'Token Price',
-      selector: 'price',
-      sortable: true,
-			cell: (row, index) => <h2 className={moment().diff(moment(row.time), 'minute') >= 1 ? 'error' : 'success'}>{ row.price }</h2>
-    },
-		{
-      name: 'Value',
-      selector: 'value',
-      sortable: true,
-			cell: (row, index) => <h2 className={moment().diff(moment(row.time), 'minute') >= 1 ? 'error' : 'success'}>{ row.value }</h2>
-    },
-		{
-			name: 'DEX',
-			selector: 'dex',
-			sortable: true,
-			cell: (row, index) => <h2 className={moment().diff(moment(row.time), 'minute') >= 1 ? 'error' : 'success'}>{ row.dex }</h2>
-		}
-  ]
-
-
-  
+//   const [alldata, setalldata] = useState([]);  
 
 // useEffect(() => {
 //     axios.fetch('https://jsonplaceholder.typicode.com/users')
@@ -186,7 +127,32 @@ const TransactionCard = () => {
 
   return (
 		<TableWrapper>
-			<DataTable columns={columns} data={data} fixedHeader={fixedHeader} fixedHeaderScrollHeight="350px"/>
+			<table>
+				<thead>
+					<tr>
+						<td>Time</td>
+						<td>Traded</td>
+						<td>Token Price</td>
+						<td>Value</td>
+						<td>DEX</td>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						data.map((val, ind) => (
+							<tr>
+								<td>
+									<Flex alignItems='center'>{ moment().diff(moment(val.time), 'minute') >= 1 ? <ArrowDown /> : <ArrowUp /> }<h2 className={moment().diff(moment(val.time), 'minute') >= 1 ? 'error' : 'success'}>{ moment().diff(moment(val.time), 'minute') >= 1 ? moment(val.time).utc().fromNow() : 'just now' }</h2></Flex>
+								</td>
+								<td><h2 className={moment().diff(moment(val.time), 'minute') >= 1 ? 'error' : 'success'}>{ val.traded }</h2></td>
+								<td><h2 className={moment().diff(moment(val.time), 'minute') >= 1 ? 'error' : 'success'}>{ val.price }</h2></td>
+								<td><h2 className={moment().diff(moment(val.time), 'minute') >= 1 ? 'error' : 'success'}>{ val.value }</h2></td>
+								<td><h2 className={moment().diff(moment(val.time), 'minute') >= 1 ? 'error' : 'success'}>{ val.dex }</h2></td>
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
 		</TableWrapper>
   )
 }
