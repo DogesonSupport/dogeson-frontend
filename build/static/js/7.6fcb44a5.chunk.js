255e54e1422a61ea6b37c8a492702d740aa6dcbc
waleed-shafiq <waleed.shafiq96@gmail.com>
2021-08-10 23:25:37 +0500

    deploy

---
+++ build/static/js/7.6fcb44a5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoose-frontend-amm"]=this["webpackJsonpgoose-frontend-amm"]||[]).push([[7],{1074:function(o,n){}}]);
+//# sourceMappingURL=7.6fcb44a5.chunk.js.map
---
+++ build/static/js/8.fc3a27f6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoose-frontend-amm"]=this["webpackJsonpgoose-frontend-amm"]||[]).push([[8],{1218:function(o,n){}}]);
+//# sourceMappingURL=8.fc3a27f6.chunk.js.map
