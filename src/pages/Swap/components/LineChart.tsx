51b3e233bcf626be0f401a1f9f105e57cae62cc2
Muhammad sufyan <msufyan833@gmail.com>
2021-08-11 21:02:11 +0500

    line chart api work

--- src/pages/Swap/components/LineChart.tsx
+++ src/pages/Swap/components/LineChart.tsx
@@ -183,7 +183,7 @@ const LineChart: React.FC<LineChartProps> = ({
         date: {since: "2021-05-01"}
         exchangeName: {in: ["Pancake", "Pancake v2"]}
         baseCurrency: {is: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"}
-        quoteCurrency: {is: "${input}"" }
+        quoteCurrency: {is: "${input}" }
       ) {
         timeInterval {
             minute(count: 5)
